c185168d0fc2acaf95bb917da33c650b94e85c1b
julian675 <jalamares23-0376@cca.edu.ph>
2025-05-09 12:07:03 +0800

    update orders

---
+++ db/ims_db.sql
@@ -0,0 +1,11 @@
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fname VARCHAR(100) NOT NULL,
+    lname VARCHAR(100) NOT NULL,
+    uname VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'viewer') NOT NULL
+);
+
+
