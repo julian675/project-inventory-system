d8912dc234d5ffdbbcdb0d3278cb0004d4b9e96f
julian675 <jalamares23-0376@cca.edu.ph>
2025-05-09 12:06:36 +0800

    das

--- js/header.js
+++
@@ -1,7 +0,0 @@
-const caretIcon = document.querySelector('.caret-section i');
-const loginBox = document.getElementById('login-box');
-
-// Toggle the visibility of the login box when the caret icon is clicked
-caretIcon.addEventListener('click', () => {
-  loginBox.style.display = loginBox.style.display === 'none' ? 'block' : 'none';
-});
